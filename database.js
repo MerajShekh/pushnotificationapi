1e4f07f9c6306fc7beba1f75fc7c13cfb5826572
Meraj Shekh <68228126+MerajShekh@users.noreply.github.com>
2022-10-18 15:59:01 +0530

    init

---
+++ database.js
@@ -0,0 +1,52 @@
+const mysql =require('mysql2/promise')
+
+const pool = mysql.createPool({
+    host:'127.0.0.1',
+    user:'root',
+    password:'',
+    database:'notification',
+
+})
+
+ async function getAllUser(){
+    try {
+        const [row] = await pool.query(`SELECT * FROM tbl_user`)
+    return row
+    } catch (error) {
+        return error.message
+    }
+}
+
+async function fetchDeviceToken(user){
+    try {
+        const [row] = await pool.query(`SELECT tDeviceToken FROM tbl_user WHERE vUserName=?`,[user])
+    return row
+    } catch (error) {
+        return error
+    }
+   
+}
+
+async function createUser(vUserName,vEmail,tDeviceToken){
+    try {
+        const [row] = await pool.query(`SELECT * FROM tbl_user where vUserName=?`,[vUserName])
+        console.log('row',row)
+        if (row.length) {
+           const [update] =  await pool.query(`UPDATE tbl_user SET tDeviceToken = ? where vUserName=?`,[tDeviceToken,vUserName])
+            console.log('Update',update.info);
+            return update.info
+        }else{
+            const [result] = await pool.query(`INSERT INTO tbl_user (vUserName,vEmail,tDeviceToken) VALUES(?,?,?)`,[vUserName,vEmail,tDeviceToken])
+            return result.insertId
+        }
+        
+        
+    } catch (error) {
+        return error.message
+    }
+    
+}
+
+
+module.exports = {getAllUser,fetchDeviceToken,createUser}
+
---
+++ index.js
@@ -0,0 +1,77 @@
+let admin =require("firebase-admin");
+const bodyParser = require('body-parser')
+const express= require('express');
+const app = express();
+const {getAllUser,fetchDeviceToken,createUser} = require("./database.js");
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+let PORT = process.env.PORT || 5000
+
+const serviceAccount = require("D:/Meraj/React Native/superchat-34597-firebase-adminsdk-serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+
+
+app.get("/users",async(req,res)=>{
+    const user = await fetchDeviceToken('user2')
+    res.send(user)
+    
+})
+
+app.post("/createUser",async(req,res)=>{
+    const {name,email,deviceToken} = req.body
+    const userId = await createUser(name,email,deviceToken)
+    res.status(200).json({message:'',userId})
+})
+
+
+app.post('/pushNotification', async (req, res)=>{
+        console.log(req.body)
+        const {title,body,user} = req.body
+
+       let token = await fetchDeviceToken(user)
+       token = token.map(e=>e.tDeviceToken)
+        let options={
+            priority:"high",
+            timeToLive:60 * 60 * 24
+        }
+    
+    
+        // let token = ['dObaT-q0QXmt3Njp6bHldO:APA91bECuw-LADMBeAwWJjFoZDs2lIvqn7JWE1_V95Lu5aQGT9PtlgrWmCTUr3Ar']
+        
+        let payload = {
+            notification:{
+                title:'fcm push notification new',
+                body:'this is the body of fcm push notification'
+            }
+        }
+
+        if (title) {
+            payload.notification.title=title
+        }
+        if (body) {
+            payload.notification.body=body
+        }
+        try {
+            admin.messaging().sendToDevice(token,payload,options)
+            .then(response=>{
+                console.log('response',response)
+                res.status(200).send(response)
+            })
+            .catch(error=>{
+                console.log("Error sending message:",error)
+                res.status(500).send(error)
+            })
+
+        } catch (error) {
+            res.status(500).send(error)
+        }
+  })
+
+  
+app.listen(PORT,()=>console.log(`API Server is running on port ${PORT}`))
